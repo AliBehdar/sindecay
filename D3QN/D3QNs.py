804b55b357359587cd4116106310a8c0fd69c91c
AliBehdar <alibehdarnejad@gmail.com>
2025-10-09 22:37:28 +0330

    result 3 just runing a simple one

--- D3QN/D3QNs.py
+++ D3QN/D3QNs.py
@@ -9,7 +9,8 @@ from collections import deque
 import os
 #from stable_baselines3.common.atari_wrappers import EpisodicLifeEnv
 from plots import plot
-from utility_off import seed,NetworkMLP
+from utility_off import seed,NetworkCNN,NetworkMLP,PrioritizedReplayBuffer
+import ale_py
 
 class DQNAgent:
     def __init__(self, env: gym.Env,cfg):
@@ -27,7 +28,7 @@ class DQNAgent:
             min_epsilon (float): min value of epsilon
             gamma (float): discount factor
         """
-        #self.device = cfg.device #torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = cfg.device #torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.cfg=cfg
         self.env = env
         self.lr=cfg.learning_rate
@@ -36,29 +37,49 @@ class DQNAgent:
         self.action_space = env.action_space
 
         self.action_space.seed(self.cfg.seed)
+        #self.state_size = self.env.observation_space.shape
         self.state_size = self.env.observation_space.shape
         self.action_size = self.env.action_space.n
 
 
-        self.dqn = NetworkMLP(self.state_size[0], self.action_size, cfg)
-        self.dqn_target = NetworkMLP(self.state_size[0], self.action_size, cfg)
+        self.is_ram = (cfg.obs_type == "ram")
+        if "ALE/" not in self.cfg.env_name:
+            self.dqn = NetworkMLP(self.state_size[0], self.action_size, cfg).to(self.device)
+            self.dqn_target = NetworkMLP(self.state_size[0], self.action_size, cfg).to(self.device) 
+        elif self.is_ram :
+            self.dqn = NetworkMLP(self.state_size[0], self.action_size, cfg).to(self.device)
+            self.dqn_target = NetworkMLP(self.state_size[0], self.action_size, cfg).to(self.device)
+        else:
+            self.dqn = NetworkCNN(self.state_size, self.action_size, cfg).to(self.device)
+            self.dqn_target = NetworkCNN(self.state_size, self.action_size, cfg).to(self.device)
         self.optimizer = optim.Adam(self.dqn.parameters(),lr=self.lr)
-        self.memory = deque(maxlen=self.cfg.memory_size)
+        #self.memory = deque(maxlen=self.cfg.memory_size)
+        self.prb=PrioritizedReplayBuffer(self.cfg.memory_size)
         self.tau = cfg.tau 
+        self.loss_history=[]
         self.gamma = cfg.gamma
         self.update_counter = 0  
+        self.is_atari = "ALE/" in self.cfg.env_name
 
     def _process_state(self, state):
         if isinstance(state, dict):
-            print("isinstance(state, dict")
             return np.concatenate([state[key].flatten() for key in sorted(state.keys())])
         else:
             return np.asarray(state).flatten()
         
     def get_action(self, state, epsilon):
 
-        state = self._process_state(state)
-        state = torch.from_numpy(np.array(state)).float().unsqueeze(0)  
+        state_np = np.array(state)
+        state = torch.from_numpy(state_np).float().to(self.device)
+        if state.dim() == 2:  # Single grayscale (h, w)
+            state = state.unsqueeze(0).unsqueeze(0)  # (1, 1, h, w)
+        elif state.dim() == 1:  # RAM (features,)
+            state = state.unsqueeze(0)  # (1, features)
+        elif state.dim() == 3 :  # Single RGB (h, w, c)
+            if self.cfg.obs_type == "rgb_image":
+                state = state.unsqueeze(0).permute(0, 3, 1, 2)  # For RGB: (1, c, h, w)
+            else:
+                state = state.unsqueeze(0)  # (1, c, h, w)     
        
         with torch.no_grad():
             q_value = self.dqn(state)[0]
@@ -69,13 +90,21 @@ class DQNAgent:
         return action
    
     def append_sample(self, state, action, reward, next_state, done):
-      
-        state = self._process_state(state)
-        next_state = self._process_state(next_state)
+        if not self.is_atari:
+            state = self._process_state(state)
+            next_state = self._process_state(next_state)
+        #state_tensor = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        #next_state_tensor = torch.FloatTensor(next_state).unsqueeze(0).to(self.device)
+        #with torch.no_grad():
+        #    q_value = self.dqn(state_tensor)[0, action]
+        #    next_q = self.dqn_target(next_state_tensor).max(1)[0].item()
+        #    target = reward + (1 - int(done)) * self.gamma * next_q
+        #    error = abs(q_value.item() - target)
         self.memory.append((state, action, reward, next_state, done))
+        #self.prb.add((state, action, reward, next_state, done), error)
     
     def train_step(self):
-
+        #batch, idxs, is_weights = self.prb.sample(self.batch_size)
         batch = random.sample(self.memory, self.batch_size)
 
         states      = np.array([i[0] for i in batch])
@@ -83,12 +112,23 @@ class DQNAgent:
         rewards     = np.array([i[2] for i in batch])
         next_states = np.array([i[3] for i in batch])
         dones       = np.array([i[4] for i in batch])
+        #is_weights = torch.FloatTensor(is_weights).to(self.device)
 
-        states      = torch.from_numpy(states).float()
-        actions     = torch.from_numpy(actions).long()
-        rewards     = torch.from_numpy(rewards).float()
-        next_states = torch.from_numpy(next_states).float()
-        dones       = torch.from_numpy(dones).float()
+        states      = torch.from_numpy(states).float().to(self.device)
+        #if not self.is_ram and self.is_atari:
+        #    if len(states.shape) == 3:  # (batch, h, w) for grayscale
+        #        states = states.unsqueeze(1)  # (batch, 1, h, w)
+            #else:  # (batch, h, w, c) for RGB
+            #    states = states.permute(0, 3, 1, 2)  # (batch, c, h, w)
+        actions     = torch.from_numpy(actions).long().to(self.device)
+        rewards     = torch.from_numpy(rewards).float().to(self.device)
+        next_states = torch.from_numpy(next_states).float().to(self.device) 
+        #if not self.is_ram and self.is_atari:
+        #    if len(next_states.shape) == 3:  # (batch, h, w) for grayscale
+        #        next_states = next_states.unsqueeze(1)  # (batch, 1, h, w)
+            #else:  # (batch, h, w, c) for RGB
+            #    next_states = next_states.permute(0, 3, 1, 2)  # (batch, c, h, w)
+        dones       = torch.from_numpy(dones).float().to(self.device)
 
         next_Qs = self.dqn(next_states)
         next_action = torch.argmax(next_Qs, dim=1)
@@ -99,29 +139,37 @@ class DQNAgent:
         target_value = rewards + self.gamma * target_value * mask
         
         curr_Qs = self.dqn(states)
-    
         main_value = curr_Qs.gather(1, actions.unsqueeze(1)).squeeze(1)
-        error = ((main_value - target_value) ** 2 / 2.0) 
+        td_error = main_value - target_value
+        error = ((td_error) ** 2 / 2.0) #* is_weights
         loss = torch.mean(error)
-
+        self.loss_history.append(loss.item())
+        
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
+        #td_errors = torch.abs(td_error).detach().cpu().numpy()
+        #for idx, err in zip(idxs, td_errors):
+        #    self.prb.update(idx, err)
+        #self.update_counter += 1
+        #if self.update_counter % self.target_updates == 0:
+         #   self._target_hard_update()
 
-        return loss.item()
+    def _target_hard_update(self):
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
     
     def _target_update(self):
-        with torch.no_grad():
-            for target_param, param in zip(self.dqn_target.parameters(), self.dqn.parameters()):
-                target_param.data.copy_(self.tau * param.data + (1.0 - self.tau) * target_param.data)
-            
+        for target_param, param in zip(self.dqn_target.parameters(), self.dqn.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1.0 - self.tau) * target_param.data)
+        
     def update_Gamma(self):
         self.gamma = 1 - 0.985 * (1 - self.gamma)
     
-    def update_epsilon(self,episode):
+    def update_epsilon(self,episode,reward):
          
         if episode <= self.cfg.max_episodes - (self.cfg.max_episodes /5):           
             if self.cfg.myAl:
+                #y = np.exp(-0.01 * episode) * (1 + 0.5 * np.cos(0.1 * episode + 0.1))
                 y = np.exp(-self.cfg.decay_rate * episode ) * (1 + self.cfg.cos_amp * np.cos(self.cfg.cos_freq * episode + self.cfg.cos_phase))
                 epsilon=self.cfg.min_epsilon + (self.cfg.max_epsilon - self.cfg.min_epsilon)* y
             else:
@@ -134,65 +182,69 @@ class DQNAgent:
 
     def load(self, path):
 
-        self.dqn = NetworkMLP(self.state_size[0], self.action_size, self.cfg).to(self.device)   
+        if self.is_ram or "ALE/" not in self.cfg.env_name:
+            self.dqn = NetworkMLP(self.state_size[0], self.action_size, self.cfg).to(self.device)
+        else:
+            self.dqn = NetworkCNN(self.state_size, self.action_size, self.cfg).to(self.device)
+            
         self.dqn.load_state_dict(torch.load(path,map_location=self.device))
-        self.dqn_target.load_state_dict(torch.load(path,map_location=self.device))
+        self._target_hard_update()
 
     def save(self, path):
         os.makedirs(os.path.dirname(path), exist_ok=True)
         torch.save(self.dqn.state_dict(), path)
 
 
-@hydra.main(version_base="1.1", config_path="./conf", config_name="config")
+@hydra.main(version_base="1.1", config_path="./conf", config_name="configs")
 def main(cfg: DictConfig):
     seed(cfg)
-    env = gym.make(cfg.env_name,render_mode="human" if not cfg.train else None,
-                   continuous=False, gravity=-9.8,enable_wind=False,
-                     wind_power=15.0, turbulence_power=1.5)
+  
+    gym.register_envs(ale_py)
+    env = gym.make(cfg.env_name,render_mode="human" if not cfg.train else None,obs_type=cfg.obs_type,frameskip=3, noop_max=30) #frame_skip
+    #env = EpisodicLifeEnv(env)
+    env=gym.wrappers.ResizeObservation(env,(84,84))
+    env = gym.wrappers.AtariPreprocessing(env, noop_max=30, frame_skip=2, screen_size=84, terminal_on_life_loss=False, grayscale_obs=True)
+    env = gym.wrappers.FrameStackObservation(env, 3)
+
     agent = DQNAgent(env,cfg)
     if cfg.train:
         update_cnt = 0
         reward_history=[]
         epsilon_history=[] 
-        loss_history =[]
         epsilon=cfg.epsilon  
         for episode in range(1,cfg.max_episodes+1):
-            state = agent.env.reset(seed=cfg.seed+episode)
+            state = agent.env.reset(seed=cfg.seed)
             state=state[0]
             episode_reward = 0
-            episode_loss=0
             done = False  
             while not done :
                 update_cnt += 1
                 action = agent.get_action(state, epsilon)
                 next_state, reward, terminated, truncated, _= agent.env.step(action)
+                #reward = np.clip(reward, -1.0, 1.0)
                 if isinstance(state, tuple): 
                     next_state = next_state[0]
-                done = terminated or truncated
                 agent.append_sample(state, action, reward, next_state, terminated)
                 state = next_state
                 episode_reward += reward
+                done = terminated or truncated
                 # if episode ends
                 if done:
                     print("Episode: {}/{}, Episodes reward: {:.6}, e: {:.3}".format(episode, cfg.max_episodes, episode_reward, epsilon)) 
                     break
-                if update_cnt >= agent.batch_size:
-                    loss=agent.train_step()
+                if update_cnt >= agent.batch_size :#and update_cnt % 4 == 0:
+                    agent.train_step()
                     agent.update_Gamma()
-                    episode_loss +=loss
-
-                    if update_cnt % agent.target_updates == 0:
-                        agent._target_update()
-            
-            loss_history.append(episode_loss)
+                    agent.update_counter += 1
+                    if agent.update_counter % agent.target_updates == 0:
+                        agent._target_hard_update()
             reward_history.append(episode_reward)   
             epsilon_history.append(epsilon)
-            
-            epsilon=agent.update_epsilon(episode)
-            
+            epsilon=agent.update_epsilon(episode,reward)
+
             if episode % cfg.save_interval==0:
                 agent.save(cfg.save_path + '_' + f'{episode}')
-                plot(episode,cfg,reward_history,epsilon_history,loss_history)
+                plot(episode,cfg,reward_history,epsilon_history,agent.loss_history)
 
     elif cfg.test:
         agent.load(cfg.load_path)
